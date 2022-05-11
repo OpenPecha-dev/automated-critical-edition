519ac715afa533ca9452982b2f4f77fb3bd038b0
kaldan007 <kaldantenzin@gmail.com>
2022-05-11 16:32:33 +0530

    archaic spelling updated

--- automated_critical_edition/detect_archaic_notes.py
+++ automated_critical_edition/detect_archaic_notes.py
@@ -133,7 +133,7 @@ def resolve_annotations(durchen):
                         continue
                     elif modern_word_pub == ann_info['default']:
                         ann_info['printable']= False
-                        ann_info['options'][modern_word_pub]["apparatus"] = ["ARCHIAC"]
+                        ann_info['options'][modern_word_pub]["apparatus"] = ["archaic"]
                     else:
                         offset = check_offset(ann_info, modern_word_pub)
                         if offset == 0:
@@ -141,7 +141,7 @@ def resolve_annotations(durchen):
                         else:
                             ann_info['printable']= False
                             ann_info['default'] = modern_word_pub
-                            ann_info['options'][modern_word_pub]["apparatus"] = ["ARCHIAC"]
+                            ann_info['options'][modern_word_pub]["apparatus"] = ["archaic"]
                             anns = update_durchen_offset(offset, anns, ann_id) 
     durchen['annotations'].update(anns)
     return durchen
@@ -162,5 +162,5 @@ def resolve_archaics(layers_path, base_path):
         global archaic_words,modern_words
         archaic_words,modern_words = get_archaic_modern_words()
         durchen = resolve_annotations(durchen)
-        new_base = get_base(durchen, load_yaml(durchen_path), base_text, "ARCHIAC")
+        new_base = get_base(durchen, load_yaml(durchen_path), base_text, "archaic")
     return new_base, durchen
--- tests/archaic_notes/test_resolve_archiac_notes.py
+++ tests/archaic_notes/test_resolve_archiac_notes.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from automated_critical_edition.detect_archiac_notes import resolve_archaics
+from automated_critical_edition.detect_archaic_notes import resolve_archaics
 from openpecha.utils import load_yaml
 
 
 
-def test_resolve_archiac_notes():
-    base_path = "./tests/archiac_notes/data/P0001//P0001.opf/base/00001.txt"
-    layers_path = "./tests/archiac_notes/data/P0001//P0001.opf/layers/"
-    new_base,resolved_archiac_durchen = resolve_archaics(layers_path, base_path)
-    expected_base_text = Path(f"./tests/archiac_notes/data/expected_base.txt").read_text(encoding='utf-8')
-    expected_durchen_output = load_yaml(Path(f"./tests/archiac_notes/data/expected_durchen.yml"))
-    assert resolved_archiac_durchen == expected_durchen_output
+def test_resolve_archaic_notes():
+    base_path = "./tests/archaic_notes/data/P0001//P0001.opf/base/00001.txt"
+    layers_path = "./tests/archaic_notes/data/P0001//P0001.opf/layers/"
+    new_base,resolved_archaic_durchen = resolve_archaics(layers_path, base_path)
+    expected_base_text = Path(f"./tests/archaic_notes/data/expected_base.txt").read_text(encoding='utf-8')
+    expected_durchen_output = load_yaml(Path(f"./tests/archaic_notes/data/expected_durchen.yml"))
+    assert resolved_archaic_durchen == expected_durchen_output
     assert new_base == expected_base_text
     
