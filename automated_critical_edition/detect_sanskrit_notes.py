90fbf2b98ce301b7b9c980fc8ca884a4f3d3dad4
Tashi Tsering <ta3tsering@gmail.com>
2022-05-25 15:00:04 +0530

    updated detect sanskrit notes

--- automated_critical_edition/detect_sanskrit_notes.py
+++ automated_critical_edition/detect_sanskrit_notes.py
@@ -1,19 +1,16 @@
-from automated_critical_edition.utils import check_all_notes_option
 from botok import WordTokenizer
 from openpecha.core.pecha import OpenPechaFS
 from automated_critical_edition.utils import update_durchen, get_base_names
+from pathlib import Path
+
 
 wt = WordTokenizer()
 
-def update_features(ann_info):
-    default = ann_info['default']
-    if ann_info["options"][default]['features']:
-        curr_features = [ann_info["options"][default]['features']]
-    else:
-        curr_features = []
-    if "SANSKRIT" not in curr_features:
-        curr_features.append("SANSKRIT")
-    ann_info['options'][default]['features'] = curr_features
+
+def update_features_for_options(ann_info, pub_types):
+    for pub, info in ann_info['options'].items():
+        if pub in pub_types:
+            info['features'] = ["SANSKRIT"]
     return ann_info
 
 def check_for_sanskrit_syl_using_botok(note):
@@ -24,38 +21,21 @@ def check_for_sanskrit_syl_using_botok(note):
     return False
 
 
-def resolve_default_sanskrit_notes(durchen):
-    anns = durchen["annotations"]
-    for ann_id, ann_info in anns.items():
-        note_options = ann_info["options"]
-        default_pub = ann_info['default']
-        all_notes = check_all_notes_option(note_options)
-        if all_notes:
-            default_note = ann_info['options'][default_pub]['note']
-            if check_for_sanskrit_syl_using_botok(default_note):
-                anns[ann_id]["printable"] = False
-                ann_info = update_features(ann_info)
-                anns[ann_id].update(ann_info)
-    durchen["annotations"].update(anns)
-    return durchen
-
-def resolve_sanskrit_optional_notes(durchen):
+def resolve_sanskrits(durchen):
     anns = durchen["annotations"]
     for ann_id, ann_info in anns.items():
         if ann_info["printable"] == True:
             pub_types = []
             note_options = ann_info["options"]
-            all_notes = check_all_notes_option(note_options)
-            default_pub = ann_info['default']
-            if all_notes:
-                for pub_type, note in note_options.items():
-                    if pub_type != default_pub:
-                        if check_for_sanskrit_syl_using_botok(note['note']):
-                            pub_types.append(pub_type)
-                if len(pub_types) >= 1:
-                    anns[ann_id]["printable"] = False
-                    ann_info = update_features(ann_info)
-                    anns[ann_id].update(ann_info)
+            for pub_type, note in note_options.items():
+                if note['note'] != "":
+                    if check_for_sanskrit_syl_using_botok(note['note']):
+                        pub_types.append(pub_type)
+
+            if len(pub_types) >= 1:
+                anns[ann_id]["printable"] = False
+                ann_info = update_features_for_options(ann_info, pub_types)
+                anns[ann_id].update(ann_info)
                 
     durchen["annotations"].update(anns)
     return durchen
@@ -67,6 +47,5 @@ def resolve_sanskrit_notes(opf_path):
     for base_name in base_names:
         durchen_layer = pecha.read_layers_file(base_name, "Durchen")
         durchen_path = pecha.layers_path / base_name / "Durchen.yml"
-        default_resolved_durchen = resolve_default_sanskrit_notes(durchen_layer)
-        sanskrit_resolved_durchen = resolve_sanskrit_optional_notes(default_resolved_durchen)
+        sanskrit_resolved_durchen = resolve_sanskrits(durchen_layer)
         update_durchen(sanskrit_resolved_durchen, durchen_path)
