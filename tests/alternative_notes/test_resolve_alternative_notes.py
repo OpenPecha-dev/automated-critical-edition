102a5f57159b33060fd9f747ae666842d61edc8e
jungtop <tenzintopjor27@gmail.com>
2022-05-27 11:40:31 +0530

    added alternatives test

---
+++ tests/alternative_notes/test_resolve_alternative_notes.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from automated_critical_edition.detect_alternative_notes import resolve_annotations,toyaml
+from automated_critical_edition.utils import get_base
+from openpecha.utils import load_yaml, dump_yaml
+
+
+def test_resolve_alternative_notes():
+    durchen_layer = load_yaml(Path('./tests/alternative_notes/data/input_durchen.yml'))
+    resolved_durchen = resolve_annotations(durchen_layer)
+    expected_durchen_output = load_yaml(Path(f"./tests/alternative_notes/data/expected_durchen.yml"))
+    assert resolved_durchen == expected_durchen_output
